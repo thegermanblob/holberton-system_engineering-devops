8edc2322c348bbbea20b4f5a5a4fd69b65229658
Thegermanblob <thegermanblob@gmail.com>
2021-08-13 22:51:09 +0000

    Make linter compliant

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { '/tmp/holberton':
-ensure  => present,
-owner   => 'www-data',
-group   => 'www-data',
-mode    => '0744',
-content => 'I love Puppet',
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
