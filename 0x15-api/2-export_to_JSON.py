a99dd5b663851758065f1508a52b3b1bba093177
Thegermanblob <thegermanblob@gmail.com>
2021-09-13 20:16:41 +0000

    Add task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from collections import OrderedDict
+import json
+import requests
+import sys
+
+
+def get_employ(emp_id):
+    """ Gets and shows employee completion rate of to-dos """
+    url = 'https://jsonplaceholder.typicode.com/'
+    emp_r = requests.get(url + 'users/{}'.format(emp_id))
+    emp_dict = emp_r.json()
+    emp_todo = requests.get(url + 'todos/', params={'userId': emp_id}).json()
+    keys = ('title', 'completed', 'username')
+    result = []
+    json_dict = dict()
+    for item in emp_todo:
+        result_item = OrderedDict()
+        item.pop('id')
+        item['username'] = emp_dict["username"]
+        for k in keys:
+            if k == "title":
+                result_item["task"] = str(item[k])
+            else:
+                result_item[k] = str(item[k])
+        result.append(result_item)
+
+    json_dict[emp_id] = result
+    with open ("{}.json".format(emp_id), "w") as jsonfile:
+        json.dump(json_dict, jsonfile)
+
+if __name__ == "__main__":
+    get_employ(sys.argv[1])
