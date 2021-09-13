692641c85b437b12c7d220366631c629f4bd4261
Thegermanblob <thegermanblob@gmail.com>
2021-09-13 17:28:01 +0000

    Make pep8 compliant

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,16 +2,19 @@
 import requests
 import sys
 
+
 def get_employ(emp_id):
+    """ Gets and shows employee completion rate of to-dos """
     url = 'https://jsonplaceholder.typicode.com/'
     emp_r = requests.get(url + 'users/{}'.format(emp_id))
     emp_dict = emp_r.json()
-    emp_todo = requests.get(url + 'todos/', params= {'userId':emp_id}).json()
+    emp_todo = requests.get(url + 'todos/', params={'userId': emp_id}).json()
     todo_comp = 0
     for item in emp_todo:
         if item['completed']:
             todo_comp += 1
-    print ('Employee {} is done with tasks({}/{}):'.format(emp_dict["name"], todo_comp, len(emp_todo)))
+    print ('Employee {} is done with tasks({}/{}):'
+           .format(emp_dict["name"], todo_comp, len(emp_todo)))
 
 
 if __name__ == "__main__":
