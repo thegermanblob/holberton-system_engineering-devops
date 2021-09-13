4ca67b02751f5730234eb8257b7d394bbc09a509
Thegermanblob <thegermanblob@gmail.com>
2021-09-13 19:36:15 +0000

    Make pep8 compliant

---
+++ 0x15-api/1-export_to_csv.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import csv
+from collections import OrderedDict
+import requests
+import sys
+
+
+def get_employ(emp_id):
+    """ Gets and shows employee completion rate of to-dos """
+    url = 'https://jsonplaceholder.typicode.com/'
+    emp_r = requests.get(url + 'users/{}'.format(emp_id))
+    emp_dict = emp_r.json()
+    emp_todo = requests.get(url + 'todos/', params={'userId': emp_id}).json()
+    keys = ('userId', 'username', 'completed', 'title')
+    result = []
+    for item in emp_todo:
+        result_item = dict()
+        item.pop('id')
+        item['username'] = emp_dict["name"]
+        for k in keys:
+            result_item[k] = str(item[k])
+        result.append(result_item)
+    with open('USER_ID.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile,
+                                fieldnames=keys, quoting=csv.QUOTE_ALL)
+        writer.writerows(result)
+
+if __name__ == "__main__":
+    get_employ(sys.argv[1])
