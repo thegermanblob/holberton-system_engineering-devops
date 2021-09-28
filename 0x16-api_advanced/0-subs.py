3c5d4bca8275309c88aa91a8ada8c7b6a0bbbd14
Thegermanblob <thegermanblob@gmail.com>
2021-09-28 13:09:53 -0400

    Add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import requests
+
+def number_of_subscribers(sub):
+    """ Gets subscriber count """
+    head = {'User-Agent': 'requested'}
+    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(sub), headers=head)
+    if r.status_code > 299:
+        return 0
+    return r.json()['data']['subscribers']
