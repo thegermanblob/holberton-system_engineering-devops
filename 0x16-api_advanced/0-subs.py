d2a8440357e75a57efcc805ba1a5cc22747c4f61
Thegermanblob <thegermanblob@gmail.com>
2021-09-28 22:39:18 -0400

    Change user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Gets subscriber count """
-    head = {'User-Agent': 'requested'}
+    head = {'User-Agent': 'pyhton3'}
     r = requests.get(
         'https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=head)
     if r.status_code > 299:
