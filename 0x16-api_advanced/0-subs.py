758adebcc7a136d9ac16296b3bdbf9cc741cec00
Thegermanblob <thegermanblob@gmail.com>
2021-09-28 22:00:24 -0400

    Fix req

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import requests
 
-def number_of_subscribers(sub):
+
+def number_of_subscribers(subreddit):
     """ Gets subscriber count """
     head = {'User-Agent': 'requested'}
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(sub), headers=head)
+    r = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=head)
     if r.status_code > 299:
         return 0
     return r.json()['data']['subscribers']
