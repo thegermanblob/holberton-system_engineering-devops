a44b2253713947827b6a13dcd4d1b2dd09810ea0
Thegermanblob <thegermanblob@gmail.com>
2021-09-28 23:29:21 -0400

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Gets subscriber count """
-    head = {'User-Agent': 'bot9000'}
-    r = requests.get(
-        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
-        headers=head, allow_redirects=False)
-    if r.status_code != 200:
+    """ returns number of subs per subreddit """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    head = {'User-Agent': 'requested'}
+    response = requests.get(url, headers=head)
+    try:
+        if response.status_code != 200:
+            return 0
+        return response.json()['data']['subscribers']
+    except:
         return 0
-    return r.json()['data']['subscribers']
