f37065191d0964799ed80366b3ff1555359cf921
Thegermanblob <thegermanblob@gmail.com>
2021-09-28 23:52:25 -0400

    Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Gets subscriber count """
-    head = {'User-Agent': 'bot9000'}
+    head = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' \
+              'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+              'Chrome/80.0.3987.87 Safari/537.36'}
     r = requests.get(
         'https://www.reddit.com/r/{}/about.json'.format(subreddit),
         headers=head, allow_redirects=False)
     if r.status_code != 200:
         return 0
     return r.json()['data']['subscribers']
+print(number_of_subscribers('funny'))
