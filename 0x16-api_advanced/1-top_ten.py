a08b48cc734fb9275ae44a83506d1826ca628209
Thegermanblob <thegermanblob@gmail.com>
2021-09-30 17:42:56 -0400

    Fix error return

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,9 @@ def top_ten(subreddit):
     """ prints the top ten hot posts for a given sub reddit """
     url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
     param = {'limit': '10'}
-    r = requests.get(url, params=param)
+    r = requests.get(url, params=param, allow_redirects=False)
     if r.status_code != 200:
-        return 0
+        return None
     r = r.json().get("data")
     for cild in r.get('children'):
         print(cild.get('data').get('title'))
