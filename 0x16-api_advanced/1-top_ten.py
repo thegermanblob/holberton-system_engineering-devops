9954cdf47ef7cbb6ce170ac259ecd5e636f530e5
Thegermanblob <thegermanblob@gmail.com>
2021-09-30 15:44:00 -0400

    Fix URL

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,10 @@ import requests
 
 def top_ten(sub):
     """ prints the top ten hot posts for a given sub reddit """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(sub)
+    url = 'https://www.reddit.com/r/{}/.json'.format(sub)
     param = {'limit': '10'}
     r = requests.get(url, params=param)
-    if r.status_code > 299:
+    if r.status_code != 200:
         return 0
     r = r.json().get("data")
     for cild in r.get('children'):
