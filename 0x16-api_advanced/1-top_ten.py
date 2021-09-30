7fc5865c2c5a64ab77e442ef9897bdf171bfbac4
Thegermanblob <thegermanblob@gmail.com>
2021-09-30 17:47:10 -0400

    Fix by adding headers

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,8 +7,9 @@ import requests
 def top_ten(subreddit):
     """ prints the top ten hot posts for a given sub reddit """
     url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
+    head = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
     param = {'limit': '10'}
-    r = requests.get(url, params=param, allow_redirects=False)
+    r = requests.get(url, headers=head, params=param, allow_redirects=False)
     if r.status_code != 200:
         return 0
     r = r.json().get("data")
