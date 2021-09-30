cdee11e2e8c10c50030edb3ac87237d27f6a86e8
Thegermanblob <thegermanblob@gmail.com>
2021-09-30 15:42:08 -0400

    Make pep8 compliatn

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Gets subscriber count """
-    head = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' \
-              'AppleWebKit/537.36 (KHTML, like Gecko) ' \
-              'Chrome/80.0.3987.87 Safari/537.36'}
+    head = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/80.0.3987.87 Safari/537.36'}
     r = requests.get(
         'https://www.reddit.com/r/{}/about.json'.format(subreddit),
         headers=head, allow_redirects=False)
